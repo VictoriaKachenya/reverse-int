ef79afa2932418b6f258baa50ec58c35d88355b4
Victoria <victoriakachenya@gmail.com>
2023-08-22 14:14:33 +0300

    feat:change js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-    const reversedNum = num => parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num)
+    function reverse(x) {
+        if (x < 0) return -reverse(-x);
+        var str = x.toString();
+        var strArray = str.split("");
+        var revArray = strArray.reverse();
+        var rev = revArray.join("");
+        return Number(rev);
+    }
 }
