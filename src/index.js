7f226cf3ef60f3ee1a03ff2af41fe278d5fc78bf
Victoria <victoriakachenya@gmail.com>
2023-08-22 14:18:19 +0300

    init: change js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function reverse (n) {
-    function reverse(x) {
-        if (x < 0) return -reverse(-x);
-        var str = x.toString();
-        var strArray = str.split("");
-        var revArray = strArray.reverse();
-        var rev = revArray.join("");
-        return Number(rev);
+    const reverse = n => {
+        const str      = "" + Math.abs(n);
+        const reversed = str.split("")
+                        .reverse()
+                        .join("");
+        const num      = +reversed;
+        return (n < 0 ? -1 : 1) * num;
     }
 }
