b4914ce79b48614c634766e08d67efdda6a5d7b2
Victoria <victoriakachenya@gmail.com>
2023-08-25 15:14:50 +0300

    feat: change mistake in answer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 module.exports = function reverse (n) {
-    const reverse = n => {
-        const str      = "" + Math.abs(n);
-        const reversed = str.split("")
-                        .reverse()
-                        .join("");
-        const num      = +reversed;
-        return (n < 0 ? -1 : 1) * num;
+    let nModule =  Math.abs(n);
+    let nString = nModule.toString();
+    let result = '';
+    for (i =(nString.length-1); i >= 0; i = i - 1) {
+    result = `${result}${nString[i]}`;
     }
-}
+    return result;
+    }
